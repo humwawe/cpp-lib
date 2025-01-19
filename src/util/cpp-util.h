fc150ef10c2a187fd64628bc6b449082c16ffe2d
hum <wawe8963098@163.com>
2025-01-19 13:26:00 +0800

    mint

--- src/util/cpp-util.h
+++ src/util/cpp-util.h
@@ -8,5 +8,10 @@
 #include <iostream>
 using namespace std;
 using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+using u128 = unsigned __int128;
+
+#include <cassert>
 
 #endif //UTIL_H

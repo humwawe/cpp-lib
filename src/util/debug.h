6b6d80570a02ffdd6367ead3534c6097bac7ffc1
hum <wawe8963098@163.com>
2025-05-02 17:47:03 +0800

    debug

--- src/util/debug.h
+++ src/util/debug.h
@@ -15,23 +15,14 @@ string to_string(const char *s) {
   return to_string((string) s);
 }
 
+string to_string(char c) {
+  return "'" + string{c} + "'";
+}
+
 string to_string(bool b) {
   return (b ? "true" : "false");
 }
 
-string to_string(vector<bool> v) {
-  bool first = true;
-  string res = "{";
-  for (int i = 0; i < static_cast<int>(v.size()); i++) {
-    if (!first) {
-      res += ", ";
-    }
-    first = false;
-    res += to_string(v[i]);
-  }
-  res += "}";
-  return res;
-}
 
 template<size_t N>
 string to_string(bitset<N> v) {
